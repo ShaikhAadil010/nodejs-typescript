8ef1b02ec3688b4a605a2c39b58797b4f48d8202
SHAIKH AADIL <shaikhaadil@SHAIKHs-MacBook-Air.local>
2025-10-02 15:44:10 +0530

    Prisma migration added

--- src/globals/routes/appRoutes.ts
+++ src/globals/routes/appRoutes.ts
@@ -1,7 +1,12 @@
 import type { Application } from 'express';
 import userRoute from '../../features/users/routes/user.routes.js';
+import type { Request, Response } from 'express';
 
 export function appRoutes(app: Application) {
   // Define your application routes here
   app.use('/api/v1/users', userRoute);
+
+  app.get('/api/v1/health', (req: Request, res: Response) => {
+    res.json({ status: 'ok' });
+  });
 }
